c1dfcd35d859f86f65fd6a4ecb18a9d8041d3200
parikhalay <parikh.alay58@gmail.com>
2024-11-13 19:50:55 -0500

    Added Crypto Files

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'antd/dist/antd.css';
+import store from './app/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
